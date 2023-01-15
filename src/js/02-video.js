a08a0552b559b5ddf4f533d3c5d3149082f43591
talka19 <talka19.10@gmail.com>
2023-01-15 19:43:08 +0200

    added 2-3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(playTime, 1000));
+function playTime(e) {
+        localStorage.setItem('videoplayer-current-time', e.seconds)
+};
+
+
+
+currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(currentTime).catch(function (error) {
+        console.error(error);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+form.addEventListener('submit', onFormSubmit);
+textareaEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+form.addEventListener('input', event => {
+    formData[event.target.name] = event.target.value; 
+    console.log(formData);
+})
+
+populateTextarea();
+
+function onFormSubmit(event){
+ event.preventDefault();
+ event.currentTarget.reset();
+ localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event){
+ const message = event.target.value;
+ localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea(){
+ const savedMessage = localStorage.getItem(STORAGE_KEY);
+ const savedMessageParse = JSON.parse(savedMessage);
+
+ if(savedMessage){
+    console.log(savedMessage);
+    textareaEl.value = savedMessageParse.email || '';
+    inputEl.value = savedMessageParse.message || ''; 
+ }
+}
